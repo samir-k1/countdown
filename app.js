23a4ef871893d1b39c0d51b821267594549a15b1
samir-k1 <110548609+samir-k1@users.noreply.github.com>
2023-04-26 19:01:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const endDate="30 April 2023 11:00 PM"
+document.getElementById("end-Date").innerText=endDate;  
+const inputs=document.querySelectorAll("input")   
+
+function clock(){
+    const end=new Date(endDate)
+    const now=new Date()
+   const diff=(end-now)/1000;
+   if(diff<0) return;
+   
+   inputs[0].value=Math.floor(diff/3600/24); 
+  
+   inputs[1].value=Math.floor(diff/3600)%24; 
+
+   inputs[2].value=Math.floor(diff/60)%60; 
+   inputs[3].value=Math.floor(diff)%60;
+}
+clock()
+//1 day=24 hr
+//1 hr=60 min
+//60 min= 3600 sec
+setInterval(
+    ()=>{
+        clock()
+    },
+    1000
+) 
